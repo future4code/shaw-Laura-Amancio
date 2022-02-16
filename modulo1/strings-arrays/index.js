56f5bee60ae531f7b1fd9fbe02f4258299923d5b
laura amancio <lauran.amancio@yahoo.com.br>
2022-02-16 16:46:58 -0300

    exercícios aula string e arrays

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,58 @@
+//Exercícios de interpretação de código
+
+// 1)
+// let array
+// console.log('a. ', array)
+// // // a. indefinido (não tem nada na array)
+
+// // array = null
+// // // console.log('b. ', array)
+// // // // b. nulo (array está vazia)
+
+// array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// console.log('c. ', array.length)
+// // // // c. 11 (número de elementos dentro da array)
+
+// let i = 0
+// console.log('d. ', array[i])
+// // // // d. 3 (aparece o primeiro elemento da array)
+
+// array[i+1] = 19
+// console.log('e. ', array)
+// // e. 3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13
+
+// const valor = array[i+6]
+// console.log('f. ', valor)
+// // f. 9
+
+// // 2)
+// const frase = prompt("Digite uma frase")
+
+// console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+// // Resposta: SUBI NUM ONIBUS EM MIRROCOS 27 
+
+// // Exercícios de escrita de código
+
+// // 1) 
+// let nomeUsuario = prompt("Qual seu nome?")
+// let emailUsuario = prompt("Qual seu email?")
+
+// console.log("O e-mail emailDoUsuario foi cadastrado com sucesso. Seja bem-vinda(o), nomeDoUsuario!")
+
+// // 2)
+
+// let arrayComidas = ["feijão", "estrogonofe", "pizza", "moqueca", "bolinho do lu"]
+// // a)
+// console.log(arrayComidas)
+
+// // 3)
+// let listaDeTarefas = [null]
+// let tarefaUm = prompt("Digite uma tarefa sua desse dia?")
+// let tarefaDois = prompt("Digite outra tarefa sua desse dia?")
+// let tarefaTres = prompt("Digite a última tarefa sua desse dia?")
+
+// let listaDeTarefas = [tarefaUm, tarefaDois, tarefaTres]
+// console.log(listaDeTarefas)
+
+// let indice = prompt("Digite qual das 3 tarefas você já realizou (de 1 a 3), por favor")
+// // vai usar aquilo do -1

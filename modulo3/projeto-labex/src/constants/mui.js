286456ca3061aca06932c9d1d9a2170519f4334e
laura amancio <lauran.amancio@yahoo.com.br>
2022-05-09 13:29:40 -0300

    caos no gitbash

---
+++ modulo3/projeto-labex/src/constants/mui.js
@@ -0,0 +1,25 @@
+import { createTheme } from '@material-ui/core/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#000000',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      light: '#292c37',
+      main: '#9f111b',
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: '#ffcc00',
+    },
+    // Used by `getContrastText()` to maximize the contrast between
+    // the background and the text.
+    contrastThreshold: 3,
+    // Usado pelas funções abaixo para mudança de uma cor de luminância por aproximadamente
+    // dois índices dentro de sua paleta tonal.
+    // Por exemplo, mude de Red 500 para Red 300 ou Red 700.
+    tonalOffset: 0.2,
+  },
+});

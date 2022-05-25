73947122eb7eca00dbbf60475e3de3de5cd20479
laura amancio <lauran.amancio@yahoo.com.br>
2022-05-25 16:34:46 -0300

    ex de typescript e coisas aleatórias q não sei de onde veio

---
+++ modulo4/intro-typescript/src/exercicio2.ts
@@ -0,0 +1,8 @@
+function imprimeTresCoresFavoritas(): void {
+    const cor1: string = "azul"
+    const cor2: string = "verde"
+    const cor3: string = "amarelo"
+    console.log([cor1, cor2, cor3])
+ }
+
+imprimeTresCoresFavoritas()
---
+++ modulo4/intro-typescript/src/exercicio4.ts
@@ -0,0 +1,18 @@
+function comparaDoisNumero(num1: number, num2: number): number {
+    let maiorNumero: number
+    let menorNumero: number
+
+    if (num1 > num2) {
+        maiorNumero = num1;
+        menorNumero = num2;
+      } else {
+        maiorNumero = num2;
+        menorNumero = num1;
+      }
+    
+      const diferenca: number = maiorNumero - menorNumero;
+    
+      return diferenca 
+}
+
+console.log(comparaDoisNumero(27, 20))

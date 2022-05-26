11e393a013ee8f138bacecb2ee7897379facef48
laura amancio <lauran.amancio@yahoo.com.br>
2022-05-26 16:29:06 -0300

    exercícios aprofundamento de typescript

---
+++ modulo4/aprofundamento-typescript/src/ex5.ts
@@ -0,0 +1,4 @@
+    // Algumas configurações que estão no slide, vem comentada no arquivo criado pelo comando no terminal, como o outDir e rootDir, responsáveis pelos arquivos de saída (colocamos no arquivo build) e os de onde vem os inputs que geral a saída (src no nosso).
+    // o sourceMap também é utilizado para fazer o .map dos arquivos que são transpilados.
+    // removeComments também é utilizado para não gerar comentários na saída de transpilação, arquivo vai limpo;
+    // noImplicitAny que nos avisa quando não tipamos o que precisa e essa leva o any como tipagem (não recomandado)
